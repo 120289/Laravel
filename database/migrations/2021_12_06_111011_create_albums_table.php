4f14f8119ad92fb2f0a3d588e8b69b0e07b0baeb
AndyLammerts <124131@student.drenthecollege.nl>
2021-12-06 12:16:23 +0100

    Albums table toegevoegd met daarin de velden 'album_name' en 'date'
    Closes #3

---
+++ database/migrations/2021_12_06_111011_create_albums_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('album_name');
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
