ac2600cec3e24f08c96af2cd1473322ac23e0d78
AndyLammerts <124131@student.drenthecollege.nl>
2021-12-06 12:22:09 +0100

    'Genre' table toegevoegd met daarin de velden 'genre_name' en 'origin_county'
    Closes #12

---
+++ app/Models/Genres.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Genres extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'genre_name','origin_country';
+    ];
+}
