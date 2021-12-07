16163edadc9266a6921005f7740972d0359712f5
120289 <120289@student.drenthecollege.nl>
2021-12-07 16:05:48 +0100

    'youtube_Videos' gemaakt met daarin de velden 'yt_name', 'yt_url'

---
+++ app/Models/Youtube_Videos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Youtube_Videos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'yt_name', 'yt_url';
+    ];
+}
