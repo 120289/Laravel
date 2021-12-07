fffe766a1de45a5fefbf7071f68a8fb8abf3817c
120289 <120289@student.drenthecollege.nl>
2021-12-07 16:02:15 +0100

    'Songs' table gemaakt met daarin de velden 'Songs_title', 'Songs_txt', 'Album_name', 'Album_Photos', 'artist_name', 'date'

---
+++ app/Models/Songs.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Songs extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'Songs_title','Songs_txt', 'Album_name', 'Album_Photos', 'artist_name', 'date';
+    ];
+}
