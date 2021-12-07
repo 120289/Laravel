2751a2da420c644d0003ef5ab35dc65c80279702
pascalhaverkamp <48521780+pascalhaverkamp@users.noreply.github.com>
2021-12-07 10:29:55 +0100

    table toegevoegd als goed is

--- app/Models/Genres.php
+++ app/Models/Genres.php
@@ -12,4 +12,9 @@ class Genres extends Model
     protected $fillable = [
       'genre_name','origin_country';
     ];
+
+    public static function contactSearch($name) {
+    return Contact::where('genre_name', 'LIKE', "%$name%")
+    ->orWhere('origin_country', 'LIKE', "%$name%")->get();
+  }
 }
