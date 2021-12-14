56337063328481e5e7e8a4d10d621ce1ea659d3b
AndyLammerts <124131@student.drenthecollege.nl>
2021-12-14 11:05:38 +0100

    Artiestencontroller heel breed ingevuld

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -4,81 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ArtistController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+class ArtistController extends Controller {
+
+    public function index(){
+      $artists = Artist::all();
+      return view('artists.index', compact ('artists'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+
+    public function create(){
+      return view('artists.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+
+    public function store(Request $request){
+        $request->validate([
+          'name' => 'required'
+        ]);
+        Album::create($request->all());
+        return redirect()->route('artists.index')->with('success', 'Deze artiest is aangemaakt!')
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+
+    public function show(Artist $artist){
+      return view('artists.show', compact('artist'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+
+    public function edit(Artist $artist){
+        return view('artists.edit', compact('artist'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+
+    public function update(Request $request, $id){
+        $request->validate([
+          'name' =>'required'
+        ]);
+        $company->update($request->all());
+        return redirect()->route('/artist')->with('succes', 'Deze artiest is aangepast!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function destroy(Artist $Artist){
+        $artist->delete();
+
+        return redirect('/artists')->with('success', 'Deze artiest is verwijderd!');
     }
 }
