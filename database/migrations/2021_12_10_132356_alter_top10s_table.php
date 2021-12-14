c3da0df6beec6c2685afcd89d3c482a857481a13
120289 <120289@student.drenthecollege.nl>
2021-12-14 14:28:10 +0100

    heb de top10s gelinkt met de users en artist albums tables

---
+++ database/migrations/2021_12_10_132356_alter_top10s_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTop10sTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('top10s', function (Blueprint $table) {
+        $table->foreignId('artist_albums_id')->constrained();
+        $table->foreignId('users_id')->constrained();
+      }
+    );
+  }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+      {
+        Schema::table('top10s', function (Blueprint $table) {
+          $table->dropForeign(['users_id']);
+          $table->dropForeign(['artist_albums_id']);
+          $table->dropColumn('users_id');
+          $table->dropColumn('artist_albums_id');
+      });
+    }
+}
